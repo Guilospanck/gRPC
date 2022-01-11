9209191efaf8dd2f017ab28aa63077c672bc016f
Guilherme Rodrigues <guilherme.rp2010@hotmail.com>
2022-01-11 19:31:15 -0300

    feat: #server implementing ListFeatures using stream and helper function

--- server/server.go
+++ server/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"math"
 
 	pb "github.com/Guilospanck/gRPC/route_guide/proto"
 	"github.com/golang/protobuf/proto"
@@ -24,7 +25,15 @@ func (s *routeGuideServer) GetFeature(ctx context.Context, point *pb.Point) (*pb
 }
 
 func (s *routeGuideServer) ListFeatures(rect *pb.Rectangle, stream pb.RouteGuide_ListFeaturesServer) error {
+	for _, feature := range s.savedFeatures {
+		if inRange(feature.Location, rect) {
+			if err := stream.Send(feature); err != nil {
+				return err
+			}
+		}
+	}
 
+	return nil
 }
 
 func (s *routeGuideServer) RecordRoute(stream pb.RouteGuide_RecordRouteServer) error {
@@ -33,4 +42,21 @@ func (s *routeGuideServer) RecordRoute(stream pb.RouteGuide_RecordRouteServer) e
 
 func (s *routeGuideServer) RouteChat(stream pb.RouteGuide_RouteChatServer) error {
 
+}
+
+// Helper function to verify if point is inside rectangle
+func inRange(point *pb.Point, rect *pb.Rectangle) bool {
+	left := math.Min(float64(rect.Lo.Longitude), float64(rect.Hi.Longitude))
+	right := math.Max(float64(rect.Lo.Longitude), float64(rect.Hi.Longitude))
+	bottom := math.Min(float64(rect.Lo.Latitude), float64(rect.Hi.Latitude))
+	top := math.Max(float64(rect.Lo.Latitude), float64(rect.Hi.Latitude))
+
+	if float64(point.Longitude) >= left &&
+		float64(point.Longitude) <= right &&
+		float64(point.Latitude) >= bottom &&
+		float64(point.Latitude) <= top {
+		return true
+	}
+
+	return false
 }
