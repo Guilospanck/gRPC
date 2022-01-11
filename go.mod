2ecbb4b57b2a0136632e98b1917b261b2aad1983
Guilherme Rodrigues <guilherme.rp2010@hotmail.com>
2022-01-11 19:23:56 -0300

    feat: #server go mod init and implementing GetFeature

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/Guilospanck/gRPC/route_guide/proto
+
+go 1.17
+
+require (
+	github.com/Guilospanck/gRPC v0.0.0-20220111220751-e852dd311365
+	github.com/golang/protobuf v1.5.2
+	google.golang.org/grpc v1.43.0
+	google.golang.org/protobuf v1.27.1
+)
+
+require (
+	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
+	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
+	golang.org/x/text v0.3.0 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+)
--- server/server.go
+++ server/server.go
@@ -4,13 +4,23 @@ import (
 	"context"
 
 	pb "github.com/Guilospanck/gRPC/route_guide/proto"
+	"github.com/golang/protobuf/proto"
 )
 
 // implements the generated RouteGuideServer (line 148 of route_guide_grpc.pb.go)
-type routeGuideServer struct{}
+type routeGuideServer struct {
+	savedFeatures []*pb.Feature
+}
 
 func (s *routeGuideServer) GetFeature(ctx context.Context, point *pb.Point) (*pb.Feature, error) {
+	for _, feature := range s.savedFeatures {
+		if proto.Equal(feature.Location, point) {
+			return feature, nil
+		}
+	}
 
+	// No feature was found, return an unnamed feature
+	return &pb.Feature{Location: point}, nil
 }
 
 func (s *routeGuideServer) ListFeatures(rect *pb.Rectangle, stream pb.RouteGuide_ListFeaturesServer) error {
