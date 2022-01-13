a55df64ed65521cf364213de5eb8f56470e409f2
Guilherme Rodrigues <guilherme.rp2010@hotmail.com>
2022-01-13 18:54:01 -0300

    feat: client and server

---
+++ TensorProgramming/BasicAPI/client/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/Guilospanck/gRPC/TensorProgramming/BasicAPI/proto"
+	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var (
+	serverAddr = flag.String("addr", "localhost:4040", "Address of the gRPC server")
+	port       = flag.Int("port", 8080, "Port of the client API")
+)
+
+func main() {
+	flag.Parse()
+
+	var opts []grpc.DialOption
+
+	// without TLS
+	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	// connects to gRPC server
+	conn, err := grpc.Dial(*serverAddr, opts...)
+	if err != nil {
+		log.Fatalf("Error while dialing to gRPC server: %v", err)
+	}
+	defer conn.Close()
+
+	// client stub (register a new client)
+	client := proto.NewAddServiceClient(conn)
+
+	// gin - creating a new server
+	router := gin.Default()
+	router.GET("/add/:a/:b", func(ctx *gin.Context) {
+		a, err := strconv.ParseUint(ctx.Param("a"), 10, 64)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Parameter A"})
+			return
+		}
+
+		b, err := strconv.ParseUint(ctx.Param("b"), 10, 64)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Parameter B"})
+			return
+		}
+
+		req := &proto.Request{A: int64(a), B: int64(b)}
+		if response, err := client.Add(ctx, req); err == nil {
+			ctx.JSON(http.StatusOK, gin.H{
+				"result": fmt.Sprint(response.Result),
+			})
+		} else {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+
+	})
+
+	router.GET("/mult/:a/:b", func(ctx *gin.Context) {
+		a, err := strconv.ParseUint(ctx.Param("a"), 10, 64)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid parameter A"})
+			return
+		}
+
+		b, err := strconv.ParseUint(ctx.Param("b"), 10, 64)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid parameter B"})
+			return
+		}
+
+		req := &proto.Request{A: int64(a), B: int64(b)}
+
+		if response, err := client.Multiply(ctx, req); err == nil {
+			ctx.JSON(http.StatusOK, gin.H{
+				"response": response.Result,
+			})
+		} else {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+
+	})
+
+	// run API
+	if err := router.Run(fmt.Sprintf(":%d", *port)); err != nil {
+		log.Fatalf("Failed to run client server: %v", err)
+	}
+}
--- TensorProgramming/BasicAPI/server/main.go
+++ TensorProgramming/BasicAPI/server/main.go
@@ -2,18 +2,67 @@ package main
 
 import (
 	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
 
 	"github.com/Guilospanck/gRPC/TensorProgramming/BasicAPI/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+var (
+	port = flag.Int("port", 4040, "The server port")
 )
 
 type server struct {
-	proto.uni
+	proto.UnimplementedAddServiceServer
 }
 
 func (s *server) Add(ctx context.Context, request *proto.Request) (*proto.Response, error) {
+	firstNum := request.A
+	secondNum := request.B
 
+	result := firstNum + secondNum
+
+	return &proto.Response{Result: result}, nil
 }
 
 func (s *server) Multiply(ctx context.Context, request *proto.Request) (*proto.Response, error) {
+	firstNum, secondNum := request.A, request.B
+
+	result := firstNum * secondNum
+
+	return &proto.Response{Result: result}, nil
+}
+
+func newServer() *server {
+	return &server{}
+}
+
+func main() {
+	flag.Parse()
+
+	listener, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
+	if err != nil {
+		log.Fatalf("Failed to start TCP server: %v", err)
+	}
+
+	var opts []grpc.ServerOption
+
+	// creates an instance of gRPC server
+	grpcServer := grpc.NewServer(opts...)
+
+	// register service implementation with the gRPC server
+	proto.RegisterAddServiceServer(grpcServer, newServer())
+
+	// reflection to serialize data
+	reflection.Register(grpcServer)
+
+	// listens
+	if err := grpcServer.Serve(listener); err != nil {
+		panic(err)
+	}
 
 }
